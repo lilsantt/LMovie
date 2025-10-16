f722347f28f83ae0b8558e4e232b01d7b789312d
lilsantt <brusnicagv@gmail.com>
2025-10-17 02:50:27 +0300

    add: loading spinner text

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,5 +1,5 @@
 import { Spinner } from "@/components/Spinner/Spinner";
 
 export default function Loading() {
-  return <Spinner text="Загрузка популярных сериалов..." />;
+  return <Spinner text="Загрузка..." />;
 }
